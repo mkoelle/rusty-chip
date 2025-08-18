979233b1a912ce3e3431bbbc36c366d9b4a8904c
Mark Koelle <mkoelle@gmail.com>
2025-08-17 19:47:07 -0500

    reproducable builds! wokwi simulation runs!

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Export ESP-IDF environment variables
+export IDF_PATH=/home/esp/esp-idf
+
+# Source ESP environment if not already done
 which idf.py >/dev/null || {
     source ~/export-esp.sh >/dev/null 2>&1
 }
+
+# Clean any previous build artifacts
+rm -rf target .embuild
 
 case "$1" in
 "" | "release")
